085d8352b753c0c5ab204aab4d62f020f7b176c0
Domingos Lucas Joanqueiro <143920842+quembojr@users.noreply.github.com>
2023-11-17 18:43:27 +0200

    wavelet1D.py

--- Haar-Wavelet discret 1D- encoder/wavelet1D.py
+++ Haar-Wavelet discret 1D- encoder/wavelet1D.py
@@ -1,4 +1,4 @@
-def WAVELET(dados):
+def encoder(dados):
     if len(dados) % 2 != 0:
         dados.append(0)
 
@@ -18,7 +18,25 @@ def WAVELET(dados):
 
     return resultado
 
+
+def decoder(dados):
+    tamanho = len(dados)
+    resultado = [0.0] * tamanho
+    nivel=2
+    while nivel <= tamanho:
+        for i in range(0, nivel, 2):
+            media = (dados[i // 2] + dados[i // 2 + nivel // 2])
+            diferenca = (dados[i // 2] - dados[i // 2 + nivel // 2])
+            resultado[i] = media
+            resultado[i + 1] = diferenca
+        for i in range(nivel):
+            dados[i] = resultado[i]
+        nivel *= 2
+    return resultado
+
+
 dados = [56, 40, 8, 24, 48, 48, 40, 16]
-resultado= WAVELET(dados)
+resultado= encoder(dados)
 print("O resultado :", resultado)
+print("A descompressao :", decoder(resultado))
 
